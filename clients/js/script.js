db1c8213c6560b5afeaa68ea9b0a70b6ff75e592
Armotea <gong322@mail.ru>
2023-11-20 22:42:09 +0300

    Функция для вывода жанров и кинематографа клиенту.

--- clients/js/script.js
+++ clients/js/script.js
@@ -1,41 +1,29 @@
-// fetch('http://127.0.0.1:8000/getGenres/')
-//   .then((response) => {
-//     data = response.json()
-//     console.log(data.Array)
-//   })
-
+cinematography = document.querySelector('.cinematography')
 category = document.querySelector('.category')
+cats = document.querySelector('.cats')
 categoryBtn = document.querySelector('.categoryBtn')
+
+
 categoryBtn.addEventListener('click', async function() {
+  category.style.display = 'flex'
   const response = await fetch("http://127.0.0.1:8000/getGenres/", {
       method: "GET",
-    });
-    result = await response.json();
-    result = JSON.stringify(result)
-    console.log(result)
-    result = JSON.parse(result)
-    console.log(result)
-    for (i of result){
-       a = category.appendChild(document.createElement('a'))
-       a.classList.add('categories')
-       a.innerHTML = i.genre_name
-       a.href = 'index.html'
-    }
-})
-// category = document.querySelector('.category')
-// async function uploadMultiple(){
-//     const response = await fetch("http://127.0.0.1:8000/getGenres/", {
-//       method: "GET",
-//     });
-//     result = await response.json();
-//     result = JSON.stringify(result)
-//     console.log(result)
-//     result = JSON.parse(result)
-//     console.log(result)
-//     for (i of result){
-//        a = category.appendChild(document.createElement('a'))
-//        a.classList.add('categories')
-//        a.innerHTML = i.genre_name
-//        a.href = 'index.html'
-//     }
-// }
+  });
+  result = await response.json();
+  for (i of result){
+    cats_li = cats.appendChild(document.createElement('li'))
+    a = cats_li.appendChild(document.createElement('a'))
+    a.innerHTML = i.genre_name
+    a.href = '#'
+    let id = i.id
+    a.addEventListener('click', async function(){
+      const response = await fetch(`http://127.0.0.1:8000/getCinematographyByGenres/{id}?genre=${id}`, {
+        method: "GET",
+      });
+      result = await response.json();
+      for (i of result){
+        h = cinematography.appendChild(document.createElement('h'))
+        h.innerHTML = i.name
+      };
+  }, {once:true})}
+}, {once:true})
